0f1b47159e85c3d5976aa575e03c352fdc8d2f12
Surya Madhavan <kmaddy9122@gmail.com>
2024-07-13 00:08:26 +0530

    update -13/07

--- easyArrays/src/FindUnion.java
+++ easyArrays/src/FindUnion.java
@@ -1,8 +1,49 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class FindUnion {
+
+    public static ArrayList<Integer> findUnionBetter(int[] arr1, int[] arr2, int n, int m){
+        int i=0, j=0;
+        ArrayList<Integer> Union = new ArrayList<>();
+        while(i < n && j < m){
+            if(arr1[i] <= arr2[j]){
+                if(Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+                    Union.add(arr1[i]);
+                i++;
+            } else {
+                if(Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+                    Union.add(arr2[j]);
+                j++;
+            }
+        }
+        while (i < n){
+            if(Union.get(Union.size()-1) != arr1[i])
+                Union.add(arr1[i]);
+            i++;
+        }
+        while (j < m) {
+            if(Union.get(Union.size()-1) != arr2[j])
+                Union.add(arr2[j]);
+            j++;
+        }
+        return Union;
+    }
+
+    public static ArrayList<Integer> findUnionWithMap(int[] arr1, int[] arr2, int n, int m) {
+        ArrayList<Integer> union = new ArrayList<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        HashSet<Integer> unionSet = new HashSet<>();
+        for(int i=0; i< n; i++)
+            unionSet.add(arr1[i]);
+//            freq.put(arr1[i], freq.getOrDefault(arr1[i], 0)+1);
+        for(int i=0; i< m; i++)
+            unionSet.add(arr2[i]);
+//            freq.put(arr2[i], freq.getOrDefault(arr2[i], 0)+1);
+        for(int it: unionSet){
+            union.add(it);
+        }
+        return union;
+    }
     public static ArrayList<Integer> findUnion(int[] arr1, int[] arr2, int n, int m) {
         ArrayList<Integer> union = new ArrayList<>();
         int minLength = Math.min(n, m);
@@ -12,7 +53,15 @@ public class FindUnion {
                 for (int j = minLength; j < maxLength; j++) {
                     int curr = maxLength == n ? arr1[j] : arr2[j];
                     if (!union.contains(curr)) {
-                        union.add(curr);
+                        int prevIndex = union.indexOf(curr - 1);
+                        int nextIndex = union.indexOf(curr + 1);
+                        if (prevIndex != -1) {
+                            union.add(prevIndex+1, curr);
+                        } else if(nextIndex != -1){
+                            union.add(nextIndex-1, curr);
+                        } else {
+                            union.add(curr);
+                        }
                     }
                 }
             } else {
@@ -34,14 +83,16 @@ public class FindUnion {
     }
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 8};
-        int[] arr2 = {1, 2, 3, 6, 7};
+        int[] arr1 = {-8, -3, -3, -2, 0, 1, 2, 2, 6};
+        int[] arr2 = {-9,-9,0};
 
         int n = arr1.length;
         int m = arr2.length;
 
         List<Integer> union = findUnion(arr1, arr2, n, m);
-        for (int i : union) {
+        List<Integer> union1 = findUnionWithMap(arr1, arr2, n, m);
+        List<Integer> union2 = findUnionBetter(arr1, arr2, n, m);
+        for (int i : union2) {
             System.out.println("i = " + i);
         }
     }
