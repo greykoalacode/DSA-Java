c3da431c99189770a346d81e57f0c6621b2b4e15
Surya Madhavan <kmaddy9122@gmail.com>
2024-09-07 15:35:02 +0530

    solved twosum again

--- leetcodePractice/src/arrays/TwoSum.java
+++ leetcodePractice/src/arrays/TwoSum.java
@@ -1,21 +1,20 @@
 package arrays;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] nums1 = {2,7,11,15};
+        int[] nums1 = {2, 7, 11, 15};
         int target1 = 9;
-        int[] nums2 = {3,2,4};
+        int[] nums2 = {3, 2, 4};
         int target2 = 6;
-        int[] nums3 = {3,3};
+        int[] nums3 = {3, 3};
         int target3 = 6;
-        int[] nums4 = {2,5,5,11};
-        int target4 = 10;
+        int[] nums4 = {2, 5, 5, 11};
+        int target4 = 11;
 
         System.out.println(Arrays.toString(twoSum(nums1, target1)));
         System.out.println(Arrays.toString(twoSumAlt(nums1, target1)));
@@ -25,22 +24,23 @@ public class TwoSum {
         System.out.println(Arrays.toString(twoSumAlt(nums3, target3)));
         System.out.println(Arrays.toString(twoSum(nums4, target4)));
         System.out.println(Arrays.toString(twoSumAlt(nums4, target4)));
+        System.out.println(Arrays.toString(twoSumNewApproach(nums4, target4)));
     }
 
     // Problem in approach: if array is sorted then its original positions are missed
     // hence result won't be correct.
     public static int[] twoSum(int[] nums, int target) {
-        int[] indices = {0,nums.length-1};
-        if(nums.length == 2) {
+        int[] indices = {0, nums.length - 1};
+        if (nums.length == 2) {
             return nums[0] + nums[1] == target ? indices : null;
         }
         Arrays.sort(nums);
         int cs = 0;
-        while(indices[0] < indices[1]){
-            cs = nums[indices[0]]+nums[indices[1]];
-            if(cs == target){
+        while (indices[0] < indices[1]) {
+            cs = nums[indices[0]] + nums[indices[1]];
+            if (cs == target) {
                 return indices;
-            } else if (cs < target){
+            } else if (cs < target) {
                 indices[0]++;
             } else {
                 indices[1]--;
@@ -49,16 +49,17 @@ public class TwoSum {
         }
         return null;
     }
+
     public static int[] twoSumAlt(int[] nums, int target) {
         Map<Integer, Integer> freqMap = new HashMap<>();
-        int[] indices = {0,1};
-        if(nums.length == 2){
-            return nums[0]+nums[1] == target ? indices : null;
+        int[] indices = {0, 1};
+        if (nums.length == 2) {
+            return nums[0] + nums[1] == target ? indices : null;
         }
 
-        for (int i=0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int curr = nums[i];
-            int req = target-nums[i];
+            int req = target - nums[i];
             if (freqMap.containsKey(req)) {
                 int reqIndex = freqMap.get(req);
                 return reqIndex < i ? new int[]{reqIndex, i} : new int[]{i, reqIndex};
@@ -66,7 +67,26 @@ public class TwoSum {
                 freqMap.put(curr, i);
             }
         }
+        return null;
+    }
 
+
+    public static int[] twoSumNewApproach(int[] nums, int target) {
+        int[] output = new int[2];
+        int j = 0;
+        while (output[0] == 0 && output[1] == 0) {
+            j++;
+            for (int i = 0; i < nums.length - j; i++) {
+                System.out.println(i+" "+(i+j));
+                if (nums[i] + nums[i + j] == target) {
+                    output[0] = i;
+                    output[1] = i + j;
+                }
+            }
+        }
+        return output;
+    }
+}
 //        int cs = 0;
 //        while(0 <= indices[0] && indices[0] < indices[1] && indices[1] < nums.length){
 //            int left = nums[indices[0]];
@@ -87,6 +107,3 @@ public class TwoSum {
 //            }
 //            System.out.println("Each case "+indices[0]+" "+indices[1]);
 //        }
-        return null;
-    }
-}
